001528e83b3304f7c9815f496c9b14d6a2ac0cc2
Carlotaml21 <carlotaml21@gmail.com>
2025-01-30 09:36:17 +0100

    first commit

---
+++ src/test/java/com/carlotaml21/MultiplicationTest.java
@@ -0,0 +1,16 @@
+package com.carlotaml21;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MultiplicationTest {
+
+    @Test
+    public void testMultiplication (){
+        assertArrayEquals(expected, actual);
+
+    }
+
+    
+}
