258165c34ac6f9e7c2585f05490fa7c31808afc5
Carlotaml21 <carlotaml21@gmail.com>
2025-01-30 09:46:26 +0100

    Add initial test for  multiplication table (TDD: RED)

--- src/test/java/com/carlotaml21/MultiplicationTest.java
+++ src/test/java/com/carlotaml21/MultiplicationTest.java
@@ -2,15 +2,30 @@ package com.carlotaml21;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 public class MultiplicationTest {
 
     @Test
     public void testMultiplication (){
-        assertArrayEquals(expected, actual);
+        List<String>expected = List.of(
+            "5 x 1 = 5",
+            "5 x 2 = 10",
+            "5 x 3 = 15",
+            "5 x 4 = 20",
+            "5 x 5 = 25",
+            "5 x 6 = 30",
+            "5 x 7 = 35",
+            "5 x 8 = 40",
+            "5 x 9 = 45",
+            "5 x 10 = 50"
+        );
 
-    }
+        List<String> result = MultiplicationTable.generateTable(5);
 
-    
+        assertEquals(expected, result);
+
+    }   
 }
